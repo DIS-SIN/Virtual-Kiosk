0c65591a29b2db1d47c05efbc0d0e6fdd27cec48
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-06-10 15:56:21 -0400

    Updates

---
+++ precache-manifest.8b9a26b75c8b7fd47449b0df797ab8a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8f455e8724ec65b84c995b488ac98b",
+    "url": "/Virtual-Kiosk/index.html"
+  },
+  {
+    "revision": "3c399641d129ad13d088",
+    "url": "/Virtual-Kiosk/static/css/main.72ac695a.chunk.css"
+  },
+  {
+    "revision": "cde32084a80a969f7971",
+    "url": "/Virtual-Kiosk/static/js/2.20b04624.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Virtual-Kiosk/static/js/2.20b04624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c399641d129ad13d088",
+    "url": "/Virtual-Kiosk/static/js/main.62ae62be.chunk.js"
+  },
+  {
+    "revision": "edb8b3069191b5e69067",
+    "url": "/Virtual-Kiosk/static/js/runtime-main.6e6b3e6d.js"
+  },
+  {
+    "revision": "287780829f7228b5abbd39dab637a947",
+    "url": "/Virtual-Kiosk/static/media/background.28778082.png"
+  },
+  {
+    "revision": "688af5e04b4472333ac4c6299cd34929",
+    "url": "/Virtual-Kiosk/static/media/kiosk.688af5e0.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Virtual-Kiosk/precache-manifest.8b9a26b75c8b7fd47449b0df797ab8a5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Virtual-Kiosk/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
